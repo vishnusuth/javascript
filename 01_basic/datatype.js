338a530d99fc9cc0c65ebef06845b14d0fbcdc55
vish-cod <manish9784666@gmail.com>
2025-09-13 23:58:14 +0530

    add initial datatype documentation with explanations of primitives and their immutability

---
+++ 01_basic/datatype.js
@@ -0,0 +1,17 @@
+// DATATYPES
+// in javascript that are serveral data types can use to store different types of data, some common data type includ :
+// number(e.g. 10,3.14), string(e.g "hello",'world'), booleans(e.g true, false), arrays(e.g [1,2,33]), objects(e.g{name: "john", age:30})
+
+//Primitive and objects
+//                     In javascript, there are two main of data : Primitive and objets
+
+//Primitives
+//          Primitives are the simplest and most basic types in javascript, They include:-->
+//          Numbers,Strings,Booleans,Null(a spical value that reperesent an absence of value),Undefind(a spical value that represents an uninitialized variables)
+
+//Primitives are immutable, which means that once they are created, they cannot be changed. For example:
+
+let x = 10;
+x = 20;
+
+//in this exmple, that value of "x" is changed from 10 to 20. However, this does not chnages that value of the primitive itself, but rather create a new primitve with the value of 20.
