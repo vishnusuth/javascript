6596f481bba05800b890b470ee83aa037c879aad
vish-cod <manish9784666@gmail.com>
2025-09-13 15:05:01 +0530

    variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,11 @@
+// in javascript variables are used to store data. They are an essentlal part of any programming language, as they part of store, and manipulate data in your programs.
+//they are a few different, each with its own syntax and rules. In this blog post, we'll take a look at the different types of variables availlable in javascript and how to use them.
+
+//==> Declaring Variables
+//                       To declare a variables in javascript. you use the var keywor followed by the name of the variable for example 
+
+
+
+var x;
+
+
