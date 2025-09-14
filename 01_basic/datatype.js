3ee572e84dd855c38bac63852e3d558aa66b0275
vish-cod <manish9784666@gmail.com>
2025-09-14 21:31:25 +0530

    ch

--- 01_basic/datatype.js
+++ 01_basic/datatype.js
@@ -15,3 +15,34 @@ let x = 10;
 x = 20;
 
 //in this exmple, that value of "x" is changed from 10 to 20. However, this does not chnages that value of the primitive itself, but rather create a new primitve with the value of 20.
+
+//Objects 
+//       objets are more complex data type in javasript and are used to represent real-world objeccts or absetract concept. Theyy are composed of key-value      pairs. where the key are string and the values can be any datatype including primmitive and other objets.
+//       objects are mutable, which means that can be changed after they are create. for example 
+
+let obj = {name:"john", age:30};
+obj.age = 31; //the age property or obj is now 31
+
+//in this example, the agr propertyy of the obj object is changed from 30 to 31. this changes the value of the object itself, rather than creating a new object.
+//there are serveral other datatype in javascript that can classified as object, including arrays, function, and dates. these data type behave similary to objects in that they are mutable after they are created.
+
+
+// operator and expression
+
+// arithmetic operators (+,-,*,/,%)
+// comparison operators (>,<,>=,<=,==,!=)
+//logical operators (&&, ||, !)
+//assignment operators (=, =+, -=, *=, |=)
+//condition operators (?!)
+
+// aritmetic operators 
+// +
+//used to add numbers or conccatente string
+
+let a = 10;
+let b = 20;
+console.log (a+b);
+
+let str1 = "hello "
+let str2 = "world"
+console.log(str1 + str2);
