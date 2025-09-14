acef52e1ed46dd1a092d629f8a59a6f2f4b755ed
vish-cod <manish9784666@gmail.com>
2025-09-14 22:03:10 +0530

    data

---
+++ 01_datatypes/comparision.js
@@ -0,0 +1,8 @@
+//comparision operators in javascript are used to compare two values. the result of a comparision is always a boolean (true o false)
+
+//equal (==)
+//compares two values for equalit, ignores datatype (does type conversion)
+
+console.log("equal");
+console.log(5=="5");
+console.log(5==5);
