ff3da854a9972d380e6450fff9e345be7fd10d78
Manish Suthar <manish9784666@gmail.com>
2025-09-15 21:48:46 +0530

    stack

---
+++ stack and heap/basic.js
@@ -0,0 +1,8 @@
+// stack and heap 
+// they are two type first is stack and second one this heap.
+// stack ke andr hamesa primitive datatype aate hai. orr heap ke andr non-primitive datatype
+// stack ko hamesa oignel data ki copy millti hai. heap ko hamesa orignal data ki ek refence value milti hhai
+
+let myname = "manish";
+let anothermyname = myname;
+console.log(anothermyname);
