4736ff43cae331f30ba9c4e9c8ecb97a6e4cb2a8
Manish Suthar <manish9784666@gmail.com>
2025-09-16 22:40:07 +0530

    array2

--- array/01_array.js
+++ array/01_array.js
@@ -1,26 +1,26 @@
 const myArr = [0,1,2,3,4]
 const myHero = ["Shaktimaan","Manish"]
 
-const myArr2 = newArr[1,2,3,4]
+const myArr2 = [1,2,3,4] // Corrected array initialization
 console.log(myArr[1]);
 
-//Array Methods
+// Array Methods
 
-myArr.push(6); //add value in last of array
-myArr.pop(); //remove value in last position
+myArr.push(6); // add value in last of array
+myArr.pop(); // remove value in last position
 myArr.unshift(9); // add value in front position
-myArr.shift() // remove value in front position
+myArr.shift(); // remove value in front position
 
-console.log(myArr.includes(9)); //true or false meh bataye gaa ki ye joo bracket mehh value dii hai wooh hai ya nahi
-console.log(myArr.index(3)); //index ki position batae gaa
+console.log(myArr.includes(9)); // true or false
+console.log(myArr.indexOf(3)); // Corrected method name
 
-const newArr = myArr.join
+const newArr = myArr.join(); // Corrected usage of join
 
 console.log(myArr);
 console.log(newArr);
 console.log(typeof newArr);
 
-//slice and splice
+// slice and splice
 
 console.log("A", myArr);
 const myn1 = myArr.slice(1,3);
