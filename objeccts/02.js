cfbfdde1f3af7b09ef9b8602d8e3728cb34edfdf
Manish Suthar <manish9784666@gmail.com>
2025-09-18 22:41:19 +0530

    obj2

--- objeccts/02.js
+++ objeccts/02.js
@@ -5,3 +5,19 @@ tinderuser.age = 18
 tinderuser.email = "manish@gmail.com"
 
 console.log(tinderuser)
+
+
+//object in object
+
+const regularUser = {
+    email : "some@gmail.com",
+    FullName : {
+        UsereFullName : {
+            FirstName : "Manish",
+            LastName : "Suthar"
+        }
+    }
+}
+
+//How to access This kind of object
+console.log(regularUser.FullName.UsereFullName.FirstName)
