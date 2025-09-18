3a7144528c510d9cce870fd711b01cde08d05b5d
Manish Suthar <manish9784666@gmail.com>
2025-09-18 22:57:28 +0530

    obj22

--- objeccts/02.js
+++ objeccts/02.js
@@ -21,3 +21,17 @@ const regularUser = {
 
 //How to access This kind of object
 console.log(regularUser.FullName.UsereFullName.FirstName)
+
+//assign
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj3 = {5:"a", 6:"b"}
+
+//litbitt use this
+const obj4 = Object.assign({}, obj1,obj2,obj3)
+console.log(obj4);
+
+//use thhis
+const obj5 = {...obj1,...obj2,...obj3}
+console.log(obj5);
