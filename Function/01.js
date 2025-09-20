dde22d1e3a2f543bccd0b397a3bcd68f77dc216f
Manish Suthar <manish9784666@gmail.com>
2025-09-20 12:46:46 +0530

    function

---
+++ Function/01.js
@@ -0,0 +1,39 @@
+/*Function functionname(){
+// function code
+}
+function()  */
+
+function name() {
+    console.log("M")
+    console.log("a")
+    console.log("n")
+    console.log("i")
+    console.log("s")
+    console.log("h")
+}
+console.log()
+
+//Adding two Number in js using function
+
+function addTwoNumber(number1, number2) {
+    console.log(number1 + number2)
+}
+addTwoNumber(3,7)
+
+//using Return
+
+function addTwoNumbers(number3, number4) {
+    let result = number3+number4
+    return result
+    //return ke baad function koi kaam nahi karta
+}
+const result = addTwoNumbers(6,7)
+console.log("Result: ", result);
+
+function loginUserMessage(username = 'sam') {
+    if (!username) {
+        console.log("Please Enter a username");
+        return
+    }
+    return`${username} just logged in`
+}
--- objeccts/02.js
+++ objeccts/02.js
@@ -28,10 +28,11 @@ const obj1 = {1:"a", 2:"b"}
 const obj2 = {3:"a", 4:"b"}
 const obj3 = {5:"a", 6:"b"}
 
-//litbitt use this
+//littlebitt use this
 const obj4 = Object.assign({}, obj1,obj2,obj3)
 console.log(obj4);
 
 //use thhis
 const obj5 = {...obj1,...obj2,...obj3}
 console.log(obj5);
+
